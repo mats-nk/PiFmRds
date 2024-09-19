9ecaf5e5e732d79c53736525b824f6db3bd91f3d
Mats Karlsson <68421483+mats-nk@users.noreply.github.com>
2024-09-19 08:17:17 +0200

    Update install.sh
    Translated to English

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# Récupérer le nom d'utilisateur courant
+# Get current username
 USER_HOME="/home/$(whoami)"
 PIFMRDS_DIR="$USER_HOME/PiFmRds"
 
-# Installer les dépendances
-echo "Installation des dépendances..."
+# Install dependencies
+echo "Install dependencies..."
 sudo apt-get update
 sudo apt-get install -y sox libsox-fmt-mp3 libsndfile1-dev
 
-# Cloner le dépôt PiFmRds
-echo "Clonage du dépôt PiFmRds dans $PIFMRDS_DIR..."
+# Clone the github repository PiFmRds
+echo "Clone the github repository PiFmRds to $PIFMRDS_DIR..."
 git clone https://github.com/DABodr/PiFmRds.git "$PIFMRDS_DIR"
 
-# Compiler PiFmRds
-echo "Compilation de PiFmRds..."
+# Compile PiFmRds
+echo "Compilation of PiFmRds..."
 cd "$PIFMRDS_DIR/src"
 make clean
 make
 
-# Rendre le script run.sh exécutable
+# Make run.sh executable
 chmod +x "$PIFMRDS_DIR/run.sh"
 
-# Indiquer que l'installation est terminée
-echo "Installation de PiFmRds terminée dans $PIFMRDS_DIR."
-echo "Vous pouvez exécuter PiFmRds avec une configuration prédéfinie en utilisant le fichier $PIFMRDS_DIR/run.sh"
+# Installation is complete
+echo "PiFmRds installation completed in $PIFMRDS_DIR."
+echo "You can run PiFmRds with a predefined configuration using the file $PIFMRDS_DIR/run.sh"
